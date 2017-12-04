5980a96a3c0cb31ebac2cf21ee6301b0bd8c4a30
KukangSulap <32661586+KukangSulap@users.noreply.github.com>
2017-12-04 14:32:39 +0700

    yhgyigi

---
+++ DownloadFreeApp/DownloadFreeApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  DownloadFreeApp
+//
+//  Created by Dora The Explorer on 12/4/17.
+//  Copyright © 2017 Dora The Explorer. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var webViewPreview: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://en.aptoide.com")
+        let request = URLRequest(url: url!)
+        
+        webViewPreview.load(request)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
